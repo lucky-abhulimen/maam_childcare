fb4f4bbead3b8d3edf82534909f0e177418f17eb
Lucky Abhulimen <giftedhands701@gmail.com>
2022-07-28 15:07:05 -0700

    first commit

---
+++ src/data/MenuData.js
@@ -0,0 +1,7 @@
+export const menuData = [
+  { title: "Home", link: "/home" },
+  { title: "About", link: "/about" },
+  { title: "Tour", link: "/tour" },
+  { title: "Info", link: "/info" },
+  { title: "Now Hiring!", link: "/now hiring" },
+];
