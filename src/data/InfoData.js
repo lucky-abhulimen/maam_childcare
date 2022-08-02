82b995724de85d924256ee93c076c7ad706c45b6
Lucky Abhulimen <giftedhands701@gmail.com>
2022-08-01 18:33:39 -0700

    created the info section

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+import ImageOne from "../images/maams.jpeg";
+import ImageTwo from "../images/daycare.jpeg";
+
+export const InfoData = {
+  heading: "Explore our beautiful classrooms",
+  paragraphOne:
+    " we love evrythin data is the b to have. thgen  we them they their we",
+  paragraphTwo: " thanks l  good work. ok faar too kind ose omoe",
+  buttonLabel: "view homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "carefully selected teachers",
+  paragraphOne:
+    " we love evrythin data is the b to have. thgen  we them they their we",
+  paragraphTwo: " thanks l  good work. ok faar too kind ose omoe",
+  buttonLabel: "view homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
